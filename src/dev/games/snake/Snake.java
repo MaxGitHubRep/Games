b6f134a1349e61ac001330bd11da5bf1288a6c4a
Max College <MC170171@reigate.ac.uk>
2017-12-18 15:01:56 +0000

    Recoded and fixed snake game.

--- src/dev/games/snake/Snake.java
+++ src/dev/games/snake/Snake.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dev.games.snake;
 
 import java.awt.Color;
@@ -10,155 +5,66 @@ import java.awt.event.KeyEvent;
 import java.util.Random;
 import javax.swing.JLabel;
 
-/**
- *
- * @author mc170171
- */
 public class Snake extends javax.swing.JFrame {
 
-    private final int SPACE = 40;
-    private final int BORDER = 400;
-    private final int ROWS = 8;
-    private final int SQUARES = ROWS*ROWS;
+    protected final int SNAKE_SIZE = 20;
+    protected final int BOARD_SIZE = 400;
+    protected final int BOARD_SQUARES = 8;
+    protected final int DEFAULT_INTERVAL = 20;
     
-    private int DIREC = 1; // 1 = UP, 2 = DOWN, 3 = RIGHT, 4 = LEFT 
-    private int INTERVAL = 0;
-    private int SCORE = 0;
-    private int SPEED = 200;
-    private int ITERATION = 0;
+    protected int direction = 3;
+    protected int speed = 200;
+    protected int defaultLength = 3;
+    protected int index = defaultLength;
+    protected int interval = 0;
     
-    private int[] historyX = new int[9999];
-    private int[] historyY = new int[9999];
-    private int[] histDirec = new int[9999];
-    JLabel[] tiles = new JLabel[SQUARES];
+    protected JLabel food = new JLabel();
     
-    Random random = new Random();
+    protected JLabel[] snake = new JLabel[BOARD_SQUARES*BOARD_SQUARES];
     
-    private boolean food = false;
-    
-    JLabel eatme = new JLabel("");
-    
-    private void moveSnake() {
+    protected void playGame() {
+        for (int i = index; i > 0; i--) {
+            snake[i].setLocation(snake[i-1].getX(), snake[i-1].getY());
+        }
+        addPosition(snake[0]);
+        correctPosition(snake[0]);
 
-        if (SCORE != 0) {
-            ITERATION++;
+        if (snake[0].getX() == food.getX() && snake[0].getY() == food.getY()) {
+            interval = DEFAULT_INTERVAL;
+            formatLabel(snake[index]);
+            index++;
+            speed = speed - 1;
         }
         
-        historyX[ITERATION] = snake.getX();
-        historyY[ITERATION] = snake.getY();
-        histDirec[ITERATION] = DIREC;
-        
-        int oldX = snake.getX();
-        int oldY = snake.getY();
-        
-        if (food == false) {
-            INTERVAL++;
-        }
-
-        addPosition(snake, DIREC);
-        for (int i = 0; i < SCORE; i++) {
-            addPosition(tiles[i], histDirec[ITERATION-i]);
-        }
-        
-        if (snake.getX() == eatme.getX() && snake.getY() == eatme.getY()) {
-            food = false;
-            INTERVAL = 10;
-            SPEED = SPEED - 10;
-            formatJLabel(tiles[SCORE], oldX, oldY);
-            SCORE++;
-        } 
-
-        correctPosition(snake);
-        for (int i = 0; i < SCORE; i++) {
-            correctPosition(tiles[i]);
-        }
-        
-        if (INTERVAL == 10) {
-
-            eatme.setBackground(Color.red);
-            eatme.setSize(SPACE, SPACE);
-            eatme.setOpaque(true);
-            eatme.setVisible(true);
-            back.add(eatme);
-            eatme.repaint();
-            eatme.setLocation(getRCoord(), getRCoord());
-
-            food = true;
-            INTERVAL = 0;
-
+        if (interval == DEFAULT_INTERVAL) {
+            formatFood();
+            interval = 0;
+        } else {
+            interval++;
         }
     }
-    
-    private void formatJLabel(JLabel label, int oldX, int oldY) {
-        label = new JLabel("");
-        label.setSize(SPACE, SPACE);
-        label.setOpaque(true);
-        label.setBackground(Color.cyan);
-        label.setLocation(oldX, oldY);
-        back.add(label);
-        label.repaint();
-    }
-    
-    private void addPosition(JLabel label, int direction) {
-        try { 
-            switch (direction) {
-                case 1:
-                    label.setLocation(label.getX(), label.getY()-SPACE);
-                    break;
-                case 2:
-                    label.setLocation(label.getX(), label.getY()+SPACE);
-                    break;
-                case 3:
-                    label.setLocation(label.getX()+SPACE, label.getY());
-                    break;
-                case 4:
-                    label.setLocation(label.getX()-SPACE, label.getY());
-                    break;
-            }
-            
-        } catch (Exception ex) {
-            System.out.println(ex);
-    }
+
+    protected void startGame() {
         
-    }
-    
-    private void correctPosition(JLabel label) {
-        try {
-            if (label.getY() >= BORDER) {
-                label.setLocation(label.getX(), 0);
-
-            } else if (label.getY() < 0) {
-                label.setLocation(label.getX(), BORDER);
-            }
-
-            if (label.getX() >= BORDER) {
-                label.setLocation(0, label.getY());
-
-            } else if (label.getX() < 0) {
-                label.setLocation(BORDER, label.getY());
-            }
-            
-        } catch (Exception ex) {
-            System.out.println(ex);
+        for (int i = 0; i < (BOARD_SQUARES*BOARD_SQUARES); i++) {
+            snake[i] = new JLabel();
         }
         
-    }
-    
-    private void startGame() {
-
+        for (int i = 0; i < defaultLength; i++) {
+            formatLabel(snake[i]);
+            snake[i].setLocation((defaultLength-1-i)*SNAKE_SIZE, 0);
+        }
+        
         Thread threads = new Thread();
-
         threads = new Thread(new Runnable() {
             public void run() {
-                
                 while (true) {
-                    moveSnake();
-                    
+                    playGame();
                     try {
-                        Thread.sleep(SPEED);
+                        Thread.sleep(speed);
                         
                     } catch (Exception ex) {
-                    
+                        ex.printStackTrace();
                     }
                 }
             }
@@ -167,9 +73,76 @@ public class Snake extends javax.swing.JFrame {
         threads.start();
     }
     
+    protected void formatLabel(JLabel label) {
+        label.setSize(SNAKE_SIZE, SNAKE_SIZE);
+        label.setBackground(Color.green);
+        label.setOpaque(true);
+        label.setVisible(true);
+        back.add(label);
+    }  
     
-    private int getRCoord() {
-        return random.nextInt(8)*SPACE;
+    protected void formatFood() {
+        food.setBackground(Color.cyan);
+        food.setSize(SNAKE_SIZE, SNAKE_SIZE);
+        food.setOpaque(true);
+        food.setVisible(true);
+        //String direc = "/dev/games/bugs/resources/foodmodels/food.fw.png";
+        //label.setIcon(new javax.swing.ImageIcon(getClass().getResource(direc)));
+        back.add(food);
+        food.setLocation(getRCoord(), getRCoord());
+
+    }
+    
+    protected int randomInt(int min, int max) {
+        return new Random().nextInt((max-min)+1)+min;
+    }
+    
+    protected int getRCoord() {
+        return randomInt(0, (BOARD_SIZE/SNAKE_SIZE))*SNAKE_SIZE;
+    }
+    
+    protected void correctPosition(JLabel label) {
+        try {
+            if (label.getY() >= BOARD_SIZE) {
+                label.setLocation(label.getX(), 0);
+
+            } else if (label.getY() < 0) {
+                label.setLocation(label.getX(), BOARD_SIZE);
+            }
+
+            if (label.getX() >= BOARD_SIZE) {
+                label.setLocation(0, label.getY());
+
+            } else if (label.getX() < 0) {
+                label.setLocation(BOARD_SIZE, label.getY());
+            }
+            
+        } catch (Exception ex) {
+            
+        }
+        
+    }
+    
+    protected void addPosition(JLabel label) {
+        try {
+            switch (direction) {
+                case 1:
+                    label.setLocation(label.getX(), label.getY()-SNAKE_SIZE);
+                    break;
+                case 2:
+                    label.setLocation(label.getX(), label.getY()+SNAKE_SIZE);
+                    break;
+                case 3:
+                    label.setLocation(label.getX()+SNAKE_SIZE, label.getY());
+                    break;
+                case 4:
+                    label.setLocation(label.getX()-SNAKE_SIZE, label.getY());
+                    break;
+            }
+            
+        } catch (Exception ex) {
+            
+        }
     }
     
     public Snake() {
@@ -177,47 +150,32 @@ public class Snake extends javax.swing.JFrame {
         startGame();
     }
 
-    /**
-     * This method is called from within the constructor to initialize the form.
-     * WARNING: Do NOT modify this code. The content of this method is always
-     * regenerated by the Form Editor.
-     */
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         back = new javax.swing.JPanel();
-        snake = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Snake");
+        setResizable(false);
         addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyPressed(java.awt.event.KeyEvent evt) {
                 formKeyPressed(evt);
             }
         });
 
-        back.setBackground(new java.awt.Color(0, 0, 0));
-
-        snake.setBackground(new java.awt.Color(102, 255, 51));
-        snake.setFont(new java.awt.Font("Agency FB", 1, 18)); // NOI18N
-        snake.setText(" ");
-        snake.setMaximumSize(new java.awt.Dimension(40, 40));
-        snake.setMinimumSize(new java.awt.Dimension(40, 40));
-        snake.setOpaque(true);
+        back.setBackground(new java.awt.Color(255, 255, 255));
 
         javax.swing.GroupLayout backLayout = new javax.swing.GroupLayout(back);
         back.setLayout(backLayout);
         backLayout.setHorizontalGroup(
             backLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(backLayout.createSequentialGroup()
-                .addComponent(snake, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 360, Short.MAX_VALUE))
+            .addGap(0, 400, Short.MAX_VALUE)
         );
         backLayout.setVerticalGroup(
             backLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(backLayout.createSequentialGroup()
-                .addComponent(snake, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 360, Short.MAX_VALUE))
+            .addGap(0, 400, Short.MAX_VALUE)
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -235,23 +193,24 @@ public class Snake extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void formKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_formKeyPressed
-        int keyCode = evt.getKeyCode();
-        switch( keyCode ) { 
+        switch(evt.getKeyCode()) { 
             case KeyEvent.VK_UP:
-                DIREC = 1;
+            case KeyEvent.VK_W:
+                direction = 1;
                 break;
             case KeyEvent.VK_DOWN:
-                DIREC = 2;
+            case KeyEvent.VK_S:
+                direction = 2;
                 break;
             case KeyEvent.VK_RIGHT:
-                DIREC = 3;
+            case KeyEvent.VK_D:
+                direction = 3;
                 break;
             case KeyEvent.VK_LEFT:
-                DIREC = 4;
+            case KeyEvent.VK_A:
+                direction = 4;
                 break;
-
         }
-
     }//GEN-LAST:event_formKeyPressed
 
     /**
@@ -291,6 +250,5 @@ public class Snake extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel back;
-    private javax.swing.JLabel snake;
     // End of variables declaration//GEN-END:variables
 }
